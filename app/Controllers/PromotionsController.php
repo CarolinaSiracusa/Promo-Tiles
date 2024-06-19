9f943ed4b5e56cfd1f1fae26bba638a72469110a
Carolina Siracusa <carolinasiracusa3@gmail.com>
2024-06-19 15:37:55 -0300

    Promotions Controller and Routes
    Creation of Promotions Controllers and Routes for initial tests

---
+++ app/Controllers/PromotionsController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Controllers;
+
+class PromotionsController extends BaseController
+{
+    public function index()
+    {
+        echo "Promotions Controller";
+        //return view('listado_productos');
+    }
+}
