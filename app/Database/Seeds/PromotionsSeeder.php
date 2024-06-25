b3a6d6912f65899d8e94567f0705a6f6b5d45721
Carolina Siracusa <carolinasiracusa3@gmail.com>
2024-06-25 14:20:44 -0300

    Seeder Modifications

--- app/Database/Seeds/PromotionsSeeder.php
+++ app/Database/Seeds/PromotionsSeeder.php
@@ -15,11 +15,11 @@ class PromotionsSeeder extends Seeder
             'description' => 'Promotion 1 description'],
             ['id' => 2,
             'title' => 'Promotion 2',
-            'image' => 'https://snov.io/blog/wp-content/uploads/2021/08/Webp.net-resizeimage3-1024x512.png',
+            'image' => 'https://t3.ftcdn.net/jpg/02/95/65/00/360_F_295650090_kEtvXGeZtWWBf28gNUmZkzFWCkiODSOo.jpg',
             'description' => 'Promotion 2 description'],
             ['id' => 3,
             'title' => 'Promotion 3',
-            'image' => 'https://snov.io/blog/wp-content/uploads/2021/08/Webp.net-resizeimage3-1024x512.png',
+            'image' => 'https://st.depositphotos.com/1518767/2699/i/450/depositphotos_26994017-stock-photo-businessman-writing-promotion.jpg',
             'description' => 'Promotion 3 description']
         ];
 
